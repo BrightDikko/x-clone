06630cceadd5d130558983fbb30c01fd06a1a98a
Bright Dikko <BDikko@nd.edu>
2023-10-10 15:00:33 -0400

    Setup springboot and configurations for spring data jpt

---
+++ backend/x-clone/src/main/java/com/xclonebackend/xclone/XCloneApplication.java
@@ -0,0 +1,13 @@
+package com.xclonebackend.xclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XCloneApplication.class, args);
+	}
+
+}
---
+++ backend/x-clone/src/test/java/com/xclonebackend/xclone/XCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xclonebackend.xclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
